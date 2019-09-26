bc41c7ea2042f89fb5e4655b7c7f1c296f300202
shenweizheng110 <1103107216@qq.com>
2019-09-26 17:05:40 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div class='main'>
+            <header class='header'>
+                <div class='container'></div>
+            </header>
+            <div class='container'>
+                <router-view />
+            </div>
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+.header {
+    height: 80px;
+    width: 100%;
+
+    .container {
+        height: 100%;
+        box-sizing: border-box;
+        border-bottom: 1px solid #dcdfe6;
     }
-  }
+}
+
+.container {
+    width: 1140px;
+    margin: 20px auto;
+    height: 100%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <el-steps :active='currentStep'>
+            <el-step title="Order" icon="el-icon-edit"></el-step>
+            <el-step title="VMachine Config" icon="el-icon-upload"></el-step>
+            <el-step title="Network Config" icon="el-icon-picture"></el-step>
+        </el-steps>
+        <div class='step-content'>
+
+        </div>
+        <div class='step-footer'>
+            <el-button class='step-footer-button' type='primary'>下一步</el-button>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            // 当前步骤
+            currentStep: 0
+        };
+    }
+};
 </script>
+
+<style lang='scss' scoped>
+.step-content {
+    width: 100%;
+    margin-top: 20px;
+    min-height: 300px;
+    border: 1px solid #EBEBEB;
+}
+
+.step-footer {
+    overflow: hidden;
+    padding-top: 20px;
+
+    .step-footer-button {
+        float: right;
+    }
+}
+</style>
